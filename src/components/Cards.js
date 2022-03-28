032991bb0f2f0e6af20211fb7eecf02c36ac4d1e
Marcos Camargo <marcoscamargo@outlook.com>
2022-03-28 20:14:52 -0300

    Changes in senteces

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,19 +6,19 @@ import "./Cards.css";
 const Cards = () => {
     return ( 
         <div className="cards" id="cards">
-            <h1>Check out these EPIC Destinations in Brazil!</h1>
+            <h1>Venha conhecer lindas paisagens no Brasil!</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem 
-                            src="adventure-website/images/bahia-1.jpg" 
-                            text="Explore the World to see whats going on out there!"
+                            src="/adventure-website/images/bahia-1.jpg" 
+                            text="Melhores destinos na Bahia!"
                             label="Chapada Diamantina - Bahia"
                             path="/products"
                         />
                         <CardItem 
-                            src="adventure-website/images/maranhao-2.jpg" 
-                            text="Explore the World to see whats going on out there!"
+                            src="/adventure-website/images/maranhao-2.jpg" 
+                            text="Melhores destinos no Maranhão!"
                             label="Lençois Maranhenses - Maranhão"
                             path="/services"
                         />
@@ -26,20 +26,20 @@ const Cards = () => {
                     </ul>
                     <ul className="cards__items">
                         <CardItem 
-                                src="adventure-website/images/rio.jpg" 
-                                text="Explore the World to see whats going on out there!"
+                                src="/adventure-website/images/rio.jpg" 
+                                text="Melhores destinos no Rio!"
                                 label="Cristo Redentor - Rio de Janeiro"
                                 path="/services"
                             />
                             <CardItem 
-                                src="adventure-website/images/cataratas.jpg" 
-                                text="Explore the World to see whats going on out there!"
+                                src="/adventure-website/images/cataratas.jpg" 
+                                text="Melhores destinos no Paraná!"
                                 label="Cataratas do Iguaçu - Paraná"
                                 path="/services"
                             />
                             <CardItem 
-                                src="adventure-website/images/genipabu.jpg" 
-                                text="Explore the World to see whats going on out there!"
+                                src="/adventure-website/images/genipabu.jpg" 
+                                text="Melhores destinos no Rio Grande do Norte!"
                                 label="Dunas de Genipabu - Rio Grande do Norte"
                                 path="/services"
                             />
