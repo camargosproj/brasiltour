c42a775f0024fbb3e5edaa8e28a404c48fa0ba38
Marcos Camargo <marcoscamargo@outlook.com>
2022-03-31 21:55:51 -0300

    Remove buttons and text when click on video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,19 +10,30 @@ const HeroSection = () => {
     const handleVideo = () => {
         setShow(!show);
     }
+
+    const handleClick = () => {
+        window.location.replace("/#cards");
+    }
+    if(!show){
     return ( 
         <div className="hero-container">
             <h1>BRASIL TOUR</h1>
             <p>Que tal conhecer o Brasil?</p>
             <div className="hero-btns">
-                <Button className="btns" goTo="/cards" buttonStyle='btn--outline' buttonSize='btn--large'>ENCONTRAR DESTINO</Button>
+                <Button className="btns" goTo="/#cards" buttonStyle='btn--outline' onClick={handleClick} buttonSize='btn--large'>ENCONTRAR DESTINO</Button>
                 <Button className="btns" goTo="/adventure-website" onClick={handleVideo} buttonStyle='btn--primary' buttonSize='btn--large'>
                     ASSISTIR VIDEO  <i className="fa fa-play"/>
                 </Button>
-            </div>
-            <VideoPlay onClose={() => setShow(false)} show={show} />
+            </div>            
         </div>
      );
+    }else{
+        return(
+            <div className="hero-container">
+                <VideoPlay onClose={() => setShow(false)} show={show} />
+            </div>
+        )
+    }
 }
  
 export default HeroSection;
